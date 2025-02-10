20ecdd91756f80e1b4b6b6c4e066c08846dff437
Jaime <jaimea-rodriguezc@unilibre.edu.co>
2025-02-10 13:18:29 -0500

    implementacion del metodo para editar persona

--- com.jaimebackend.primeraweb/src/main/java/com/jaimebackend/primeraweb/controller/PersonaController.java
+++ com.jaimebackend.primeraweb/src/main/java/com/jaimebackend/primeraweb/controller/PersonaController.java
@@ -5,10 +5,7 @@ import com.jaimebackend.primeraweb.entities.Persona;
 import com.jaimebackend.primeraweb.service.PersonaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,21 +19,39 @@ public class PersonaController {
     // la clase model se utiliza para transferir objetos del controller a la vista
     @GetMapping
     public String listarPersonas(Model model){
-     List<Persona> personas = personaService.obtenerTodas();
-     model.addAttribute("personasLista", personas);
-     return "listar";
+        List<Persona> personas = personaService.obtenerTodas();
+        model.addAttribute("personasLista", personas);
+        return "listar";
     }
 
     @GetMapping("/nueva")
     public String mostrarFormularioDeNuevaPersona(Model model){
-      model.addAttribute("persona", new Persona());
-      model.addAttribute("accion","/personas/nueva");
+        model.addAttribute("persona", new Persona());
+        model.addAttribute("accion","/personas/nueva");
         return"formulario";
     }
 
     @PostMapping("/nueva")
-    public String guarsarNuevaPersona(@ModelAttribute Persona persona){
+    public String guardarNuevaPersona(@ModelAttribute Persona persona){
         personaService.crearPersona(persona);
         return "redirect:/personas";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String mostrarFormularioEditarPersona(@PathVariable Long id, Model model) {
+        Persona persona = personaService.obtenerPorId(id);
+        if (persona == null) {
+            return "redirect:/personas"; // Evita errores si no se encuentra la persona
+        }
+        model.addAttribute("persona", persona);
+        model.addAttribute("accion", "/personas/editar/" + id);
+        return "formulario";
+    }
+
+    @PostMapping ("/editar/{id}")
+    public String actualizarPersonas(@PathVariable Long id, @ModelAttribute Persona persona){
+        personaService.actualizarPersona(id,persona);
+        return "redirect:/personas";
+
     }
 }
